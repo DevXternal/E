7647baa304a5465043fc77f645697aa00eddaa00
Josh <92058789+Obesto@users.noreply.github.com>
2022-06-08 21:10:13 -0700

    Create cryt.php

---
+++ cryt.php
@@ -0,0 +1,19 @@
+<html>
+<head>
+<title>Lipex Studios: CryT</title>
+<link rel="stylesheet" href="epic.css">
+<link rel="icon" type="image/x-icon" href="favicon.ico">
+</head>
+<body>
+  <div class="topnav">
+  <a href="index.php">Home</a>
+  <a class="active" href="cryt.php">CryT</a>
+  <a href="scripts.php">Script Hub</a>
+  <a href="https://github.com/Lipex-Studios">GitHub</a>
+  </div>
+  <h1>CryT</h1>
+  <h2>A simple roblox script executor written in C#</h2>
+  <p>CryT is an exploit developed by <a href="https://github.com/Obesto">Obesto</a> and is open-sourced on GitHub, it was made using Visual Studio and WRD-API, you can
+    find the repository <a href="https://github.com/Obesto/CryT">here.</a>
+</body>
+</html>
