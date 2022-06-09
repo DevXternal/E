273328daea41097cc1f7f1677631200c27c7f8ee
Josh <92058789+DevXternal@users.noreply.github.com>
2022-06-09 06:53:20 -0700

    Create scripts.php

---
+++ scripts.php
@@ -0,0 +1,24 @@
+<html>
+<head>
+<title>Lipex Studios: Script Hub</title>
+<link rel="stylesheet" href="epic.css">
+<link rel="icon" type="image/x-icon" href="favicon.ico">
+  <style>
+a {
+color: #ffffff;
+}
+</style>
+</head>
+<body>
+  <div class="topnav">
+  <a href="index.php">Home</a>
+  <a href="cryt.php">CryT</a>
+  <a class="active" href="scripts.php">Script Hub</a>
+  <a href="https://github.com/Lipex-Studios">GitHub</a>
+  </div>
+  <h1>Script Hub</h1>
+  <h2>Here you can share scripts and cool stuff</h2>
+  <button class="button"><span><a href="https://forms.office.com/Pages/ResponsePage.aspx?id=DQSIkWdsW0yxEjajBLZtrQAAAAAAAAAAAAMAAEYRZYFUNUhSU05HWURGTEs3QUE0R0VVWURJM0dYNC4u">Submit Script </a></span></button>
+  <div><h1>Scripts</h1></div>
+</body>
+</html>
