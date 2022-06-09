c3dec0bde4f2d93fced66da8841e17367233fc01
Josh <92058789+Obesto@users.noreply.github.com>
2022-06-08 20:48:36 -0700

    Update index.php

--- index.php
+++ index.php
@@ -4,5 +4,11 @@
 <link rel="stylesheet" href="epic.css">
 </head>
 <body>
+  <div class="topnav">
+  <a class="active" href="index.php">Home</a>
+  <a href="cryt.php">CryT</a>
+  <a href="scripts.php">Script Hub</a>
+  <a href="https://github.com/Lipex-Studios">GitHub</a>
+  </div>
 </body>
 </html>
